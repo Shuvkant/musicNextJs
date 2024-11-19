86151774c759dcdd97bd60e89b5a50de4671e55e
Shuvkant <weird426344@gmail.com>
2024-11-19 19:25:54 +0545

    adding heroSection

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,10 @@
 import Image from "next/image";
+import HeroSection from "@/components/HeroSection.tsx";
 
 export default function Home() {
-  return <h1 className="text-2xl text-center">Shuvkant Chaudhary Phanait</h1>;
+  return (
+    <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
+      <HeroSection />
+    </main>
+  );
 }
